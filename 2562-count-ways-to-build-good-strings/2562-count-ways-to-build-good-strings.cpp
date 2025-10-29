711c01856f2d522ecda3d80893ae8e32d9dec0e5
ManishRana0725 <manishbadm0725@gmail.com>
2025-10-29 16:32:20 +0530

    Time: 9 ms (35.08%), Space: 11.7 MB (78.83%) - LeetHub

--- 2562-count-ways-to-build-good-strings/2562-count-ways-to-build-good-strings.cpp
+++ 2562-count-ways-to-build-good-strings/2562-count-ways-to-build-good-strings.cpp
@@ -28,8 +28,30 @@ public:
         return dp[i] = ans;
     }
     int countGoodStrings(int low, int high, int zero, int one) {
-        dp.assign(high +1 , -1);
-        return solve(0 , low , high , zero , one);
+        dp.assign(high +1 , 0);
+        //return solve(0 , low , high , zero , one);
+
+        for(int i= low ; i<= high ; i++)dp[i] = 1;
+
+
+        for(int i=high-1; i>=0 ; i--){
+            int ans = 0;
+
+            if(zero + i <= high ){
+                ans +=  dp[i+zero];
+                ans %= MOD;
+            }
+            if(one + i <= high ){
+                ans +=  dp[i+one];
+                ans %= MOD;
+            }
+            
+            if(i >= low && i <= high)ans++;
+            ans %= MOD;
+            dp[i] =  ans;
+        }
+
+        return dp[0];
         
     }
 };
