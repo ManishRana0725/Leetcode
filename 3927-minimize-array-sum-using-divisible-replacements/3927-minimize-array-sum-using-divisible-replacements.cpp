5f90302e2a3598896be849ff2def11d8061dd77a
ManishRana0725 <manishbadm0725@gmail.com>
2026-05-14 01:06:36 +0530

    Time: 59 ms (94.53%), Space: 249.2 MB (24.54%) - LeetHub

--- 3927-minimize-array-sum-using-divisible-replacements/3927-minimize-array-sum-using-divisible-replacements.cpp
+++ 3927-minimize-array-sum-using-divisible-replacements/3927-minimize-array-sum-using-divisible-replacements.cpp
@@ -5,7 +5,7 @@ public:
         int maxi = *max_element(nums.begin(), nums.end());
         
         int n = nums.size();
-       
+
         vector<int> freq(maxi+1 , 0);
         
         for(int i=0 ; i<n ; i++){
