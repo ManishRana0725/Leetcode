d48456082395e24846c6fc03d7652cad9df87d9a
ManishRana0725 <manishbadm0725@gmail.com>
2026-08-02 19:09:21 +0530

    Time: 17 ms (58.52%), Space: 136.4 MB (87.93%) - LeetHub

---
+++ 4012-count-of-unfinished-tasks-after-each-shift/4012-count-of-unfinished-tasks-after-each-shift.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    vector<int> countTasks(vector<int>& tasks, vector<int>& shifts) {
+        
+        int n = tasks.size();
+        int m = shifts.size();
+
+        vector<long long> prefix(n+1 , 0);
+
+        for(int i=n-1 ; i>=0 ; i--){
+
+            prefix[i] = tasks[i] + prefix[i+1];
+            
+        }
+
+        int i=0 ; 
+        vector<int> ans(m);
+
+        long long remaining = tasks[0];
+
+        for(int j=0 ; j<m ; j++){
+
+            long long total = remaining + prefix[i] - tasks[i];
+
+            if((long long)shifts[j] >= total){
+                // push back 0 
+                // i = 0 , remaining = 0
+                ans[j] = 0;
+                i = 0 , remaining = tasks[0]; 
+            }else{
+                long long diff = total - shifts[j];
+
+                // binaray on prefix array as diff is target 
+
+                int l = i, r = n - 1;
+                int pos = i;
+
+                while (l <= r) {
+                    int mid = (l + r) / 2;
+
+                    if (prefix[mid] >= diff) {
+                        pos = mid;
+                        l = mid + 1;
+                    } else {
+                        r = mid - 1;
+                    }
+                }
+
+                i = pos;
+                remaining = diff - prefix[pos + 1];
+
+                ans[j] = n - i;
+
+
+            }
+        }
+
+        return ans;
+        
+    }
+};
