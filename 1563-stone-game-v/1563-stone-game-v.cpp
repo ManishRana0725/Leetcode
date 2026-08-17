92d25332d56f460c48f42d8b5fe91d4e6cf203e4
ManishRana0725 <manishbadm0725@gmail.com>
2026-08-17 15:49:07 +0530

    Time: 423 ms (55.42%), Space: 27.9 MB (17.02%) - LeetHub

---
+++ 1563-stone-game-v/1563-stone-game-v.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    
+    int solve(int l , int r , vector<int>& prefix , vector<vector<int>>& dp){
+
+        if(l >= r )return 0;
+        
+        int ans = 0;
+        if(dp[l][r] != -1)return dp[l][r];
+
+        for(int i = l ; i <= r-1 ; i++){
+
+            int leftsum = prefix[i] - ((l-1 >= 0) ? prefix[l-1] : 0);
+            int rightsum = prefix[r] - prefix[i];
+
+            if(leftsum < rightsum){
+                ans = max(ans , leftsum + solve(l , i , prefix , dp));
+            }else if(rightsum < leftsum){
+                ans = max(ans , rightsum + solve(i+1 , r , prefix , dp));
+            }else{
+                ans = max(ans , leftsum + max(solve(l , i , prefix , dp) , solve(i+1 , r , prefix , dp)));
+            } 
+        }
+
+        return dp[l][r] = ans;
+
+    }
+    int stoneGameV(vector<int>& stoneValue) {
+        int n = stoneValue.size();
+
+        vector<int> prefix(n , 0);
+        prefix[0] = stoneValue[0];
+        for(int i=1; i<n ; i++){
+            prefix[i] = stoneValue[i] + prefix[i-1] ;
+        }
+
+        vector<vector<int>> dp(n+1 , vector<int>(n+1 , -1));
+
+        return solve(0 , n-1 , prefix , dp);
+    }
+};
